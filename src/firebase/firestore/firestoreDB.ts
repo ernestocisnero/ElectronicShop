171824342d25ee4f7a151bfe4210b8f2fd1561fe
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-12-15 20:29:36 -0500

    Add products list and images

--- src/firebase/firestore/firestoreDB.ts
+++ src/firebase/firestore/firestoreDB.ts
@@ -9,14 +9,14 @@ export const firestoreDB = async () => {
     await setDoc(doc(citiesRef, "userfdkfiklonvb8676482"), {
         cart_items: [
             {
-            productID: "lkhadfhafhdkj982398498",
-            count: 20
-        },
-        {
-            productID: "33333333333333ffffffffffffff",
-            count: 3
-        }
-    ]
+                productID: "lkhadfhafhdkj982398498",
+                count: 20
+            },
+            {
+                productID: "33333333333333ffffffffffffff",
+                count: 3
+            }
+        ]
     }
     );
 
@@ -24,13 +24,13 @@ export const firestoreDB = async () => {
     const querySnapshot = await getDoc(doc(firestoreCartDB, "user_carts", "userfdkfiklonvb8676482"));
 
     const data = querySnapshot.data();
-    data?.cart_items.map( (item: any) =>{
+    data?.cart_items.map((item: any) => {
         console.log(item);
-        console.log("Product ID",item.productID, "Count", item.count);
-        
-        
-    } )
+        console.log("Product ID", item.productID, "Count", item.count);
 
-    
-    
+
+    })
+
+
+
 }
